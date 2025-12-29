8cc5f9c543107ed1fb07825181725620a75b8041
Luis Felipe Cusi Castro <cusiluisito@gmail.com>
2025-12-29 19:53:14 -0400

    pre produccion

--- terra-canada/src/app/core/services/pago.service.ts
+++ terra-canada/src/app/core/services/pago.service.ts
@@ -78,7 +78,7 @@ export interface WebhookModalEvent {
   providedIn: 'root'
 })
 export class PagoService {
-  private apiUrl = 'http://localhost:3000/api/v1/pagos';
+  private apiUrl = 'https://terra-canada-test-back.vamw1k.easypanel.host/api/v1/pagos';
   private pagosSubject = new BehaviorSubject<PagoDisplay[]>([]);
   public pagos$ = this.pagosSubject.asObservable();
   private webhookModalSubject = new Subject<WebhookModalEvent>();
